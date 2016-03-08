69d49351a62ddc8cb150248863eed0d03ec287da
various.authors <various.authors@gmail.com>
2016-03-08 12:48:02 -0800

    fiddled withmap layout

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,9 +3,13 @@ class Location < ActiveRecord::Base
 
   attr_readonly :address, :phone, :service_id
 
-  geocoded_by :address
+  geocoded_by :full_address
   after_validation :geocode
 
   validates :address, presence: true, allow_blank: false
   validates :phone, presence: true, allow_blank: false
+
+  def full_address
+    self.address + " Vancouver, BC CANADA"
+  end
 end
